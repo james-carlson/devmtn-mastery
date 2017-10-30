6ab590e9a983c30dcc1c183f0896d15f42d44c80
James Carlson <carlson.james@gmail.com>
2017-10-30 16:01:20 -0600

    more changes

--- harveyhelp/src/components/Feed/Feed.js
+++ harveyhelp/src/components/Feed/Feed.js
@@ -6,6 +6,24 @@ import { connect } from 'react-redux';
 import { loadFeed } from '../../ducks/reducer';
 
 class Feed extends Component {
+
+    // This is the componentDidMount lifecycle hook. 
+    // It fires when the component mounts. That's technically after the render function. 
+    // They fall into several different categories.
+    // Mounting: When a component is being created and inserted into the DOM
+            // constructor()
+            // componentWillMount()
+            // render()
+            // componentDidMount()
+    // Updating: Generally speaking, an update to a component happens when there is a change to props or state.
+            // componentWillReceiveProps()
+            // shouldComponentUpdate()
+            // render()
+            // componentDidUpdate()
+    // There's also componentWillUnmount and componentDidCatch.
+    // ComponentDidMount fires when the DOM has rendered the component. It's usually a good place to put things like
+    // calls to an API or other async actions related to the backend, etc. 
+
     componentDidMount(){
         this.props.loadFeed();
     }
